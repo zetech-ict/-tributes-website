2220a17405cad6549e46c00ee999293a17ec914e
dereque254 <117070550+dereque254@users.noreply.github.com>
2023-09-29 10:10:55 -0700

    Add files via upload
    tribute website

---
+++ index.hh
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tribute to Lucky Dube</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Tribute to Lucky Dube
+        </h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#about">Lucky Philip Dube (pronounced duu-beh;[1] 3 August 1964 – 18 October 2007) was a South African reggae musician and Rastafarian. His record sales across the world earned him the Best Selling African Musician prize at the 1996 World Music Awards. In his lyrics, Dube discussed issues affecting South Africans and Africans in general to a global audience. He recorded 22 albums in a 25-year period and was Africa's best-selling reggae artist of all time.[2][3] Dube was murdered in the Johannesburg suburb of Rosettenville on the evening of 18 October 2007.</a></li>
+            <li><a href="#achievements">Achievements</a></li>
+            <li><a href="#legacy">Legacy</a></li>
+        </ul>
+    </nav>
+    <section id="about">
+        <h2>About Lucky Dube</h2>
+        <p>Lucky Dube was born in Ermelo, Transvaal (now Mpumalanga), on 3 August 1964. His parents separated before his birth, and he was raised by his mother, who named him "Lucky" because she considered his birth fortunate after a number of failed pregnancies..</p>
+    </section>
+    <section id="achievements">
+        <h2>Lucky Philip Dube  3 August 1964 – 18 October 2007) was a South African reggae musician and Rastafarian. His record sales across the world earned him the Best Selling African Musician prize at the 1996 World Music Awards</h2>
+        <ul>
+            <li>winning nobe award.</li>
+            <li>Another Lucky Philip Dube was a South African reggae musician and Rastafarian. His record sales across the world earned him the Best Selling African Musician prize..</li>
+        </ul>
+    </section>
+    <section id="legacy">
+        <h2>Legacy</h2>
+        <p>On 21 October 2008, Rykodisc released a compilation album entitled Retrospective, which featured many of Dube's most influential songs as well as tracks previously not released in the United States. The album celebrated Dube's music and honored the contributions he made to South Africa. The Roots Reggae Library has taken steps to store digital versions of the artist's mbaqange albums made in the 1980s. Five of the six albums have been retrieved; Ngikwethembe Na has yet to be found. As one of the first artists to bring African reggae to the mainstream, Dube bridged cultural gaps within the African diaspora. What his music did was "[present] a praxis of cross-culturality and visionary possibility".Dube gave Africa a voice and put its culture on the global stage by joining the global reggae community. Through taking Jamaican music back to its roots, he recontextualized the oppression and political struggles that reggae seeps itself in, bringing the basis of the diaspora back in conversation with the diaspora at large to allow for a more pan-African form of cultural expression. Dube's roots reggae brought African people to the table in terms of conversation about the black diaspora by mimicking Caribbean artists' assertions of African authenticity.
+            On 18 October 2017, Gallo Records South Africa released a 25-track limited-edition commemorative album titled The Times We've Shared. The album features Dube's biggest hits, exclusive performances, and three previously unreleased tracks.
+            In Australia, Dube's music has found resonance in remote Aboriginal communities, and his popularity has led him to be called "Bigger than the Beatles" throughout much of central and northern Australia
+            </p>
+    </section>
+    <footer>
+        <p>Copyright © 2023 Derick Obwaro</p>
+    </footer>
+</body>
+</html>
